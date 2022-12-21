70b7af967c465dd18808d16fb60c1ac2e0771ea5
cryptopavelsan <99785705+cryptopavelsan@users.noreply.github.com>
2022-12-21 02:27:00 -0500

    Update index.ts
    Further refinement

--- src/index.ts
+++ src/index.ts
@@ -131,7 +131,7 @@ const jupiterSwap = async ({
               
 const main = async () => {
   try {
-    console.log(setcolour("Starting Jupiter V3 DCA Bot",92));
+    console.log(setcolour("Starting Jupiter V4 DCA Bot",92));
     console.log("The bot will retry "+String(tradingRetries)+" times if the swap fails for each scheduled period.");
     
     const cluster = "mainnet-beta"; // Force mainnet, as this uses Jupiter which is not deployed on devnet/testnet
